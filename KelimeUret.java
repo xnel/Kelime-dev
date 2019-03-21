5443f4ae52cf2a36e7efe6f9e8eec31a19c6018b
xnel <48160120+xnel@users.noreply.github.com>
2019-03-21 09:08:33 +0300

    Add files via upload

---
+++ KelimeUret.java
@@ -0,0 +1,53 @@
+package odev;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class KelimeUret {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Kelime kac karakterli olacak?");
+
+		int kacKarakter = input.nextInt();
+
+		System.out.println("Kac tane kelime olusturulacak?");
+
+		int kacKelime = input.nextInt();
+		
+
+		Random rand = new Random(26);
+		
+
+		char[] alfabe = new char[26]; // karakterleri tutacak olan array tanimlandi.
+		
+		for (char ch = 'A'; ch <= 'Z'; ++ch) 
+		{
+			alfabe[ch - 'A'] = ch; // alfabe dizisi harflerle dolduruldu.
+		}
+
+		System.out.println(alfabe); // alfabe yazdirildi.
+		
+		
+
+		for (int i = 1; i <= kacKelime; i++) {
+			System.out.println();
+			
+			for (int k = 1; k <= kacKarakter; k++) {
+
+				int x = rand.nextInt(26);
+				System.out.print(alfabe[x]);
+				
+			}
+
+		}
+		
+		
+		
+		
+
+	}
+
+}
